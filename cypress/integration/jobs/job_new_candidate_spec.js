93efbf979ab6a7c0ff081c4c7989d4557ef74c2d
QAEngineerRPallierTemp <qaengineerrpalliertemp@protonmail.com>
2020-02-06 23:45:26 +0100

    Début des tests de création

--- cypress/integration/jobs/job_new_candidate_spec.js
+++ cypress/integration/jobs/job_new_candidate_spec.js
@@ -5,7 +5,18 @@ describe('The new candidate page', () => {
 		cy.visit('/dashboard/o/dkxzma3/jobs/2XMOI_yq66e6b/new-candidate/392777')
 	})
 
-	it('greets with Ajouter un nouveau candidat', () => {
-		cy.contains('h1', 'Ajouter un nouveau candidat')
+	it('greets with the creation modal', () => {
+		//Header
+		cy.get('.modal-content h1')
+    	  .should('contain', 'Ajouter un nouveau candidat')
+    	  .and('have.class', 'card-form-title')
+
+    	cy.get('.modal-content a i')
+    	  .should('have.class', 'card-form-close')
+
+    	//Form
+    	cy.get('card-form-form').then(($cardForm) => {
+		 	
+		})
 	})
 })
